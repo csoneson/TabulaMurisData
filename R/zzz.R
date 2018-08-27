3e53730e7c6148ebbd7536132141f7ce6ecfd36f
Charlotte Soneson <charlottesoneson@gmail.com>
2018-08-27 09:28:38 +0200

    Imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 #' @importFrom utils read.csv
+#' @importFrom ExperimentHub createHubAccessors
 .onLoad <- function(libname, pkgname) {
     fl <- system.file("extdata", "metadata.csv", package = "TabulaMurisData")
     titles <- utils::read.csv(fl, stringsAsFactors = FALSE)$Title
